11d546f8639f83c2bc72671f63f5d7fcecdb0845
ebrahimam <ebrahim.ros78@gmail.com>
2025-01-30 20:55:21 +0330

    Create Gready Folder

--- Data_Structure/Tree.js
+++ Data_Structure/Tree.js
@@ -1,60 +1,29 @@
-// *********************TREE*****************
 
 
-class TreeNode {
-      constructor(value) {
-            this.value = value;
+class TreeNode{
+      constructor(key,value=key,parent=null) { 
+            this.key =key;
+            this.value =value;
+            this.parent =parent;
             this.children = []
       }
-      addChild(child) {
-            this.children.push(child)
+
+      get isLeaf(){
+            return this.children.length ===0
+      }
+
+      get hasChildren(){
+            return !this.isLeaf;
       }
 }
 
-class Tree {
-      constructor(rootValue) {
-            this.root = new TreeNode(rootValue)
+class Tree{
+      constructor(key,value=key) {
+            this.root = new TreeNode(key,value);
       }
-
-      //pre-order traversal
-      traversePreOrder(node = this.root) {
-            node.children.forEach(child => this.traversePreOrder(child))
+      
+      *preOrederTraversal(node=this.root) { 
+            yield node
       }
-
-      //post-order traversal
-      traversePostOrder(node = this.root) {
-            node.children.forEach(child => this.traversePostOrder(child))
-      }
-
-      //level-order
-      traverseLevelOrder() {
-            const queue = [this.root]
-            while (queue.length > 0) {
-                  const node = queue.shift();
-                  queue.push(...node.children)
-            }
-      }
-
 }
 
-
-//example 
-const tree = new Tree('Root');
-const child1 = new TreeNode('Child 1');
-const child2 = new TreeNode('Child 2');
-
-tree.root.addChild(child1);
-tree.root.addChild(child2);
-child1.addChild(new TreeNode('Child 1.1'));
-child1.addChild(new TreeNode('Child 1.2'));
-child2.addChild(new TreeNode('Child 2.1'));
-
-// Traversals
-console.log('Pre-order traversal:');
-tree.traversePreOrder();
-
-console.log('Post-order traversal:');
-tree.traversePostOrder();
-
-console.log('Level-order traversal:');
-tree.traverseLevelOrder();
